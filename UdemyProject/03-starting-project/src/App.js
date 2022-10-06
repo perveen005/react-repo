ffc96d0e0de1367331023b3abac71227f7ab5382
Praveen <praveen.tiwari@neudesic.com>
2022-10-06 13:34:30 +0530

    adding use reducer for optimizing the code for password and value

--- UdemyProject/03-starting-project/src/App.js
+++ UdemyProject/03-starting-project/src/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect} from 'react';
 
-import Login from './components/Login/Login';
+import { Login } from './components/Login/Login';
 import Home from './components/Home/Home';
 import MainHeader from './components/MainHeader/MainHeader';
 
