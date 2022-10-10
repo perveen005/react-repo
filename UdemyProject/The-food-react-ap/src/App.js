6d123df8c07ccbfb7672fa40b37818039e1ca406
Praveen <praveen.tiwari@neudesic.com>
2022-10-10 09:02:33 +0530

    add cart

--- UdemyProject/The-food-react-ap/src/App.js
+++ UdemyProject/The-food-react-ap/src/App.js
@@ -1,15 +1,25 @@
 import { Header } from "./components/Layout/Header";
+import { useState } from "react";
 import { Meals } from "./components/Meals/Meals";
 import { Cart } from "./components/Cart/Cart";
+import { CartProvider } from "./store/CartProvider";
 function App() {
+  const [cartIsShown,setCartIsShown] = useState(false);
+  const showCartHandler = () => {
+     setCartIsShown(true)}
+  const hideCartHandler = () => {
+     setCartIsShown(false)}
+   
+
+  
   return (
-    <>
-     <Cart/>
-     <Header/>
+    <CartProvider>
+     {cartIsShown &&<Cart onClose= {hideCartHandler}/>}
+     <Header onShowCart= {showCartHandler}/>
      <main>
       <Meals/>
      </main>
-    </>
+    </CartProvider>
   );
 }
 
