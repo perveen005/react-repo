ef1dc3c56862c7027f2fcf43e25192431a04993b
Praveen <praveen.tiwari@neudesic.com>
2022-10-17 20:39:09 +0530

    authentication app completed

--- UdemyProject/Main-spa-routing-app/src/App.js
+++ UdemyProject/Main-spa-routing-app/src/App.js
@@ -1,14 +1,22 @@
 import { Route, Switch, Redirect } from 'react-router-dom';
-
-import AllQuotes from './pages/AllQuotes';
-import QuoteDetail from './pages/QuoteDetail';
-import NewQuote from './pages/NewQuote';
-import NotFound from './pages/NotFound';
+import React, {Suspense} from 'react'
 import Layout from './components/layout/Layout';
+import LoadingSpinner from './components/UI/LoadingSpinner';
+
+const NewQuote =React.lazy(() => import('./pages/NewQuote'));
+const QuoteDetail =React.lazy(() => import('./pages/QuoteDetail'));
+const NotFound =React.lazy(() => import('./pages/NotFound'));
+const AllQuotes =React.lazy(() => import('./pages/AllQuotes'));
 
 function App() {
   return (
     <Layout>
+      <Suspense
+      fallback = {
+        <div className='centered'>
+         <LoadingSpinner/>
+        </div>
+      }>
       <Switch>
         <Route path='/' exact>
           <Redirect to='/quotes' />
@@ -26,6 +34,7 @@ function App() {
           <NotFound />
         </Route>
       </Switch>
+      </Suspense>
     </Layout>
   );
 }
