b020c755bd4cb5e4f4762a2555292831dd669605
Praveen <praveen.tiwari@neudesic.com>
2022-10-17 14:39:38 +0530

    learning navigation and revision

--- UdemyProject/Advanced Redux/src/components/Shop/ProductItem.js
+++ UdemyProject/Advanced Redux/src/components/Shop/ProductItem.js
@@ -10,6 +10,7 @@ const ProductItem = (props) => {
   const { title, price, description, id } = props;
 
   const addToCartHandler = () => {
+    
     dispatch(
       cartActions.addItemToCart({
         id,
---
+++ UdemyProject/react-typescript/with-typescript.js
@@ -0,0 +1,5 @@
+function adds(a, b) {
+    return a + b;
+}
+var results = add(2, 5);
+console.log(results);
